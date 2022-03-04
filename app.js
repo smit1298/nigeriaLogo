e99a27569936994135aea518dfc771f367248d80
smit1298 <61621188+smit1298@users.noreply.github.com>
2022-03-04 14:10:53 +0100

    images added
    nigeria logos images implemented

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-const selectElement = document.querySelector('select')
+const selectElement = document.querySelector("select");
 console.log(nigeriaLogo.length);
 
-for ( let i = 0; i < nigeriaLogo.length; i++){
-    const optionElement = document.createElement("option");
-    optionElement.textcontent = nigeriaLogo[i].category;
-    optionElement.setAttribute("value", i);
-    optionElement.setAttribute("id", i);
-    selectElement.insertAdjacentElement("beforeend", optionElement);
+
+let sortedCategories = categories.sort()
+
+for (let i = 0; i < sortedCategories.length; i++) {
+  const optionElement = document.createElement("option");
+  optionElement.textContent = sortedCategories[i];
+  optionElement.setAttribute("value", i);
+  optionElement.setAttribute("id", i);
+  selectElement.insertAdjacentElement("beforeend", optionElement);
 }
 
 
-
-
-
-
-
+for ( let i = 0; i < nigeriaLogo.length; i++){
+const fileName = nigeriaLogo[i].fileName; 
+console.clear()
+console.log(nigeriaLogo[i]);
+const fileImage = document.createElement("img")
+fileImage.setAttribute("src", `./logos/${fileName}/${fileName}.png`)
+}

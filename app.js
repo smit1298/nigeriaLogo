adf4da0e2e8b64c3775dad5df248638d49f371c7
smit1298 <61621188+smit1298@users.noreply.github.com>
2022-03-09 23:26:20 +0100

    nigeriaLogo clickables
    clickable select options and alphabets

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const selectElement = document.querySelector("select");
 const imageForAll = document.getElementById("images");
 const logoContainer = document.getElementById("image-container");
 const allAlphabets = document.getElementById("alphabets");
+const anchor = document.getElementsByTagName("p");
 
 let sortedCategories = categories.sort();
 
 for (let i = 0; i < sortedCategories.length; i++) {
   const optionElement = document.createElement("option");
   optionElement.textContent = sortedCategories[i];
-  optionElement.setAttribute("value", i);
+  optionElement.setAttribute("value", sortedCategories[i]);
   optionElement.setAttribute("id", i);
   selectElement.insertAdjacentElement("beforeend", optionElement);
 }
@@ -22,8 +23,13 @@ for (let i = 0; i < sortedCategories.length; i++) {
 
 // const alpha = Array.from(Array(26)).map((e, i) => i + 65);
 // const alphabet = alpha.map((x) => String.fromCharCode(x));
-const alphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
+
+
+const alphabet = "#abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
+
 console.log(alphabet);
+
+
 for (let i = 0; i < alphabet.length; i++) {
   const paragraphElement = document.createElement("p");
   paragraphElement.textContent = alphabet[i];
@@ -40,9 +46,9 @@ function pickOutCompany(companyEntity) {
   <div class="card-box"> <div class="card"> 
   <img src="${logoUrl}" alt="${fileName}" class="card-image"> 
   </div> 
-  <p class="title">
+  <div class="title">
    <a href="" >${title}</a> 
-   </p> 
+   </div> 
    <h3> 
    <span class="category">${category}</span>
    </h3>
@@ -52,3 +58,36 @@ function pickOutCompany(companyEntity) {
 for (let i = 0; i < nigeriaLogo.length; i++) {
   pickOutCompany(nigeriaLogo[i]);
 }
+
+
+
+document.getElementById("categories").addEventListener("change", function(e){
+  let categoryValue = this.value;
+  logoContainer.innerHTML = '';
+
+  for (let i = 0; i < nigeriaLogo.length; i++){
+    if (nigeriaLogo[i].category.includes(categoryValue)){
+      pickOutCompany(nigeriaLogo[i]);
+    }
+  }
+})
+
+// making all alphabets clickable
+
+
+for (let i = 0; i < anchor.length; i++) {
+  anchor[i].addEventListener("click", function (e) { 
+    const presentLetter = e.currentTarget.textContent;
+    logoContainer.innerHTML = "";
+
+    for (let i = 0; i < nigeriaLogo.length; i++) 
+    {
+      if (nigeriaLogo[i].title.startsWith(presentLetter)) 
+      {
+        pickOutCompany(nigeriaLogo[i]);
+      }
+    }
+  }
+  );
+}
+
